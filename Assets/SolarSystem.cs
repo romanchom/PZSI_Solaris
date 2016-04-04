8b47656498d5723950fcde51d0fe541b297061fc
romanchom <romanchom@gmail.com>
2016-04-04 20:26:34 +0200

    Zalążek tworzenia systemu.

---
+++ Assets/SolarSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SolarSystem : MonoBehaviour {
+	[SerializeField]
+	private int[] levelPopMin;
+	[SerializeField]
+	private int[] levelPopMax;
+
+	[SerializeField]
+	private GameObject satellitePrefab;
+
+	// Use this for initialization
+	void Start () {
+		createSystem(gameObject, 0);
+	}
+
+	void createSystem(GameObject parent, int depth) {
+		if (depth >= levelPopMin.Length) return;
+		int bodyCount = Random.Range(levelPopMin[depth], levelPopMax[depth] + 1);
+		for(int i = 0; i < bodyCount; ++i) {
+			GameObject satellite = Instantiate<GameObject>(satellitePrefab);
+			satellite.transform.parent = parent.transform;
+			createSystem(satellite, depth + 1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
